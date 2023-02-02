d7bff4c5206cb4402ca28c7833b7d74329ffc18f
gybran <charistomarcgybran@students.undip.ac.id>
2023-02-02 14:35:24 +0700

    chart realtime & detail with range)

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -50,6 +50,17 @@
                         <h3>{{ $rectifier->name }}</h3>
                         <h4>{{ $rectifier->ip_recti }}</h4>
                         <p>Type Rectifier</p>
+                        <a href="#" class="realtime-btn mb-5" data-bs-toggle="modal" data-id="{{ $rectifier->ip_recti }}" data-bs-target="#modalRecti">
+                            <span class="link-text">
+                                Realtime
+                                <svg width="25" height="16" viewBox="0 0 25 16" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M17.8631 0.929124L24.2271 7.29308C24.6176 7.68361 24.6176 8.31677 24.2271 8.7073L17.8631 15.0713C17.4726 15.4618 16.8394 15.4618 16.4489 15.0713C16.0584 14.6807 16.0584 14.0476 16.4489 13.657L21.1058 9.00019H0.47998V7.00019H21.1058L16.4489 2.34334C16.0584 1.95281 16.0584 1.31965 16.4489 0.929124C16.8394 0.538599 17.4726 0.538599 17.8631 0.929124Z"
+                                        fill="#753BBD" />
+                                </svg>
+                            </span>
+                        </a>
                         <a href="#" class="detail-btn" data-bs-toggle="modal" data-id="{{ $rectifier->ip_recti }}" data-bs-target="#modalRecti">
                             <span class="link-text">
                                 Details
@@ -71,22 +82,13 @@
 
         {{-- MODALS  --}}
         <div class="modal fade" id="modalRecti" data-bs-backdrop="static" tabindex="-1" aria-labelledby="modalRecti" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-xl">
+            <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-xl">
                 <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5">Judul Modal</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal"
-                            aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Close</button>
-                    </div>
+                    image.png
                 </div>
             </div>
         </div>
+
     </div>
 
     {{-- PAGINATION  --}}
@@ -95,23 +97,7 @@
     </div>
 
     <script>
-
-        $(".detail-btn").click(function(){
-            const ip_recti = $(this).attr('data-id');
-            console.log(ip_recti);
-
-            $.ajax({
-                url: "rectifier/"+ip_recti,
-                type: 'GET', 
-                success: function(data) {
-                    $('.modal-body').html(data);
-                },
-                error: function(data) {
-                    console.log(data);
-                }
-            })
-        })
-        
+        // TOAST
         @if (session()->has('success'))
             Toastify({
                 text: "{{ session('success') }}",
@@ -126,27 +112,46 @@
             }).showToast();
         @endif
 
-        var chartData = {
-            labels: ['17:00', '18.00', '19.00', '20.00', '21.00'],
-            datasets: [{
-                    label: "Voltage",
-                    data: [35.5, 45.1, 33.2, 35.3, 36.4],
-                    borderColor: "red",
-                    fill: false
-                },
-                {
-                    label: "Current",
-                    data: [54.3, 44.9, 52.7, 34.9, 35.7],
-                    borderColor: "blue",
-                    fill: false
-                },
-                {
-                    label: "Temperature",
-                    data: [33.5, 43.1, 37.2, 39.3, 40.4],
-                    borderColor: "green",
-                    fill: false
-                }
-            ]
-        };
+
+        // AJAX MODAL DETAIL
+        $(".realtime-btn").click(function () {
+            const ip_recti = $(this).attr("data-id");
+
+            try {
+                $.ajax({
+                    url: "rectifier/realtime/" + ip_recti,
+                    type: "GET",
+                    success: function (data) {
+                        $(".modal-content").html(data);
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    },
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
+        // AJAX MODAL DETAIL
+        $(".detail-btn").click(function () {
+            const ip_recti = $(this).attr("data-id");
+
+            try {
+                $.ajax({
+                    url: "rectifier/detail/" + ip_recti,
+                    type: "GET",
+                    success: function (data) {
+                        $(".modal-content").html(data);
+                    },
+                    error: function (data) {
+                        console.log(data);
+                    },
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        });
+
     </script>
 @endsection

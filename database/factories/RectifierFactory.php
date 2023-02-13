7141422907249a6a1cf40e888f783e63b27571d3
gybran <charistomarcgybran@students.undip.ac.id>
2023-02-13 10:41:20 +0700

    fix: showAnalysis

--- database/factories/RectifierFactory.php
+++ database/factories/RectifierFactory.php
@@ -16,8 +16,20 @@ class RectifierFactory extends Factory
      */
     public function definition()
     {
+        $type = ['TTC', 'Inner', 'Outer'];
         return [
-            //
+            'name' => $this->faker->name,
+            'site_name' => $this->faker->name,
+            'rtpo' => $this->faker->word,
+            'nsa' => $this->faker->word,
+            'type' => $type[mt_rand(0, 2)],
+            'ip_recti' => $this->faker->ipv4(),
+            'community' => 'fake community',
+            'version' => 1,
+            'port' => 161,
+            'oid_voltage' => $this->faker->ipv6(),
+            'oid_current' => $this->faker->ipv6(),
+            'oid_temp' => $this->faker->ipv6(),
         ];
     }
 }
